657b4b12154ca0d0cce0feefa551387a58ad130c
Ling YAO <ling@propic.com.au>
2020-04-24 22:41:59 +1000

    update links and basic styles

--- js/btns.js
+++ js/btns.js
@@ -1,23 +1,23 @@
 window.onload = function(){
     var homebtn = document.getElementById("homebtn");
     homebtn.onclick = function(){
-        window.location = "/index.html";
+        window.location = "../index.html";
     }
     var newsbtn = document.getElementById("newsbtn");
     newsbtn.onclick = function(){
-        window.location = "/page/news.html"
+        window.location = "../page/news.html"
     }
     var productsbtn = document.getElementById("productsbtn");
     productsbtn.onclick = function(){
-        window.location = "/page/products.html"
+        window.location = "../page/products.html"
     }
     var storebtn = document.getElementById("storebtn");
     storebtn.onclick = function(){
-        window.location = "/page/store.html"
+        window.location = "../page/store.html"
     }
     var aboutUsbtn = document.getElementById("aboutUsbtn");
     aboutUsbtn.onclick = function(){
-        window.location = "/page/aboutUs.html"
+        window.location = "../page/aboutUs.html"
     }
 
 
